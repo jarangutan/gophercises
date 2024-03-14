4b966e82bac1b2dbd79b1528158916773d94246b
Jarangutan <53702919+jarangutan@users.noreply.github.com>
2024-03-14 13:59:19 -0400

    cleaned up link.go and the tests

--- link/link.go
+++ link/link.go
@@ -1,32 +1,48 @@
 package link
 
 import (
-	"golang.org/x/net/html"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type Link struct {
-	Href string
-	Text string
+	href string
+	text string
 }
 
-func FindA(n *html.Node, links *[]Link) {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		t := strings.TrimSuffix(n.FirstChild.Data, "\n")
-
-		var h string
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				h = a.Val
-			}
-		}
-
-		*links = append(*links, Link{
-			Href: h,
-			Text: t,
-		})
+func grabText(n *html.Node, s string) string {
+	if n.Type == html.TextNode {
+		s = s + " " + n.Data
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		FindA(c, links)
+		s = grabText(c, s)
 	}
+	return strings.TrimSpace(s)
+}
+
+func grabAnchor(n *html.Node) Link {
+	var h string
+	for _, a := range n.Attr {
+		if a.Key == "href" {
+			h = a.Val
+		}
+	}
+	t := grabText(n, "")
+
+	return Link{
+		href: h,
+		text: t,
+	}
+}
+
+func FindAnchors(n *html.Node, links []Link) []Link {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		// t := strings.TrimSuffix(n.FirstChild.Data, "\n")
+		links = append(links, grabAnchor(n))
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = FindAnchors(c, links)
+	}
+	return links
 }
--- link/link_test.go
+++ link/link_test.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"fmt"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -8,26 +9,105 @@ import (
 	"testing"
 )
 
-func TestFindSimpleLink(t *testing.T) {
-	input := `<html>
-  <body>
-    <h1>Hello!</h1>
-    <a href="/other-page">A link to another page</a>
-  </body>
-</html>`
-
-	doc, err := html.Parse(strings.NewReader(input))
+func createNode(content string) *html.Node {
+	n, err := html.Parse(strings.NewReader(content))
 	if err != nil {
 		panic(err)
 	}
+	return n
+}
 
-	var links []Link
-	FindA(doc, &links)
+func createAnchorNode(href string, text string) *html.Node {
+	textNode := createNode(text)
+	attrs := make([]html.Attribute, 1)
+	attrs[0] = html.Attribute{
+		Namespace: "",
+		Key:       "href",
+		Val:       href,
+	}
 
-	println(links[0].Href)
-	println(links[0].Text)
+	return &html.Node{
+		Parent:      nil,
+		FirstChild:  textNode,
+		LastChild:   textNode,
+		PrevSibling: nil,
+		NextSibling: nil,
+		Type:        html.ElementNode,
+		DataAtom:    0,
+		Data:        "a",
+		Namespace:   "",
+		Attr:        attrs,
+	}
+}
 
-	if links[0].Href != "/other-page" {
-		t.Error("Nope")
+func Test_grabText_JustText(t *testing.T) {
+	expected := "some text"
+	node := createNode(expected)
+
+	result := grabText(node, "")
+	if result != expected {
+		t.Errorf("Result of \"%v\" did not match expected \"%v\"", result, expected)
+	}
+}
+
+func Test_grabText_TextInsideSpan(t *testing.T) {
+	expected := "some text inside a span"
+	node := createNode(fmt.Sprintf("<span>%v</span>", expected))
+
+	result := grabText(node, "")
+	if result != expected {
+		t.Errorf("Result of \"%v\" did not match expected \"%v\"", result, expected)
+	}
+}
+
+func Test_grabText_TextInsideSpanWithComments(t *testing.T) {
+	expected := "some text inside a span"
+	node := createNode(fmt.Sprintf("<span><!-- Hello I am a comment :) -->%v</span>", expected))
+
+	result := grabText(node, "")
+	if result != expected {
+		t.Errorf("Result of \"%v\" did not match expected \"%v\"", result, expected)
+	}
+}
+
+func Test_grabText_TextInAnchorSplitBySpan(t *testing.T) {
+	expected := "some text split by things"
+	node := createNode(fmt.Sprintf(`<div>
+      <a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">some text split
+        <span>by <!--multiple--></span>things
+      </a>
+    <div>
+    `))
+
+	result := grabText(node, "")
+	if result != expected {
+		t.Errorf("Result of \"%v\" did not match expected \"%v\"", result, expected)
+	}
+}
+
+func Test_grabAnchor_JustAnchor(t *testing.T) {
+	expected := Link{"/other-page", "A link to a page!"}
+	anchorNode := createAnchorNode(expected.href, expected.text)
+
+	result := grabAnchor(anchorNode)
+	if result != expected {
+		t.Errorf("Result of \"%+v\" did not match expected \"%+v\"", result, expected)
+	}
+}
+
+func TestFindSimpleLink(t *testing.T) {
+	expected := Link{"/other-page", "A link to a page!"}
+
+	node := createNode(`
+    <html>
+      <body>
+        <a href="/other-page">A link to a page!</a>
+      </body>
+    </html>
+    `)
+
+	result := FindAnchors(node, make([]Link, 0))
+	if result[0] != expected {
+		t.Errorf("Result of \"%+v\" did not match expected \"%+v\"", result, expected)
 	}
 }
